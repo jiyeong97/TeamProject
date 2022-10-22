9a4a9481d0eda84737b2ea6b10acb95daf83892d
jiyeong97 <j.yun087@mybvc.ca>
2022-10-22 13:05:54 -0600

    JY commit

--- Pages/registration.js
+++ Pages/registration.js
@@ -3,8 +3,8 @@ import React from 'react';
 const registration = () => {
     return (
         <div>
-            <div>
-                <h1 className='title'>BVC Course Registration</h1>
+            <div className='title'>
+                <h1>BVC Course Registration</h1>
             </div>
             <div className='menu'>
                 {/* <Link className='diplomaMenu'>Diploma</Link>
@@ -12,6 +12,22 @@ const registration = () => {
                 <Link className='certificateMenu'>Certificate</Link>
                 <Link className='upgradeMenu'>Upgrade</Link>
                 <Link className='myCourseMenu'>My Course</Link> */}
+            </div>
+            <div className='programDiv'>
+                <div>Programs</div>
+                <div className='programCheck'>
+                    <form>
+                        <input type="checkbox" id="SotwareDevelopment"/>
+                        <label for="SotwareDevelopment">Software Development</label>
+                        <input type="checkbox" id="InformationTechnologySystem"/>
+                        <label for="InformationTechnologySystem">Information Technology System</label>
+                        <input type="checkbox" id="InteriorDecorating"/>
+                        <label for="InteriorDecorating">Interior Decorating</label>
+                        <input type="checkbox" id="DigitalDesign"/>
+                        <label for="DigitalDesign">Digital Design</label>
+                    </form>
+                    
+                </div>
             </div>
         </div>
     );
