5bff6628529e5eb1d354a319c1fa2525931db538
jiyeong97 <j.yun087@mybvc.ca>
2022-10-22 12:23:17 -0600

    second commit

--- App.js
+++ App.js
@@ -1,4 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter,Routes ,Route } from 'react-router-dom';
 
+import logIn from './Pages/log-in';
+import registration from './Pages/registration';
+
+class App extends Component{
+    render(){
+        return(
+            <>
+            <BrowserRouter>
+            <Routes>
+                <Route index elemet={<logIn/>} exact/>
+                <Route path="/registration" element={<registration/>}/>
+            </Routes>
+            </BrowserRouter>
+            </>
+        );
+    }
+}
+
 export default App;
