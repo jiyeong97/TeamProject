9d018c3eb75ff29c34dc8ec0d19f012e266bbcb0
jiyeong97 <j.yun087@mybvc.ca>
2022-10-24 21:46:28 -0600

    master

--- Pages/log-in.js
+++ Pages/log-in.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import '../css/login.css';
+import logo from '../images/Logo.svg'
 
-const logIn = () => {
+const LogIn = () => {
     return (
-        <div>
+          
+    <div>
+
+        <header className='header'>
+              <img  className='imagen' src={logo} alt="this is car image" />
+        </header>
+ 
+
+        <h2 className='title'>Welcome to Bow Valley College!</h2>
+        <div> <h2 className='title-loginpage'>Login Page</h2></div>
+            <div className='container'>
+           
+                 <form>
+                
+                <input type="text" name="name"  placeholder='Name'/>
+               
+                <input type="password" name="password" placeholder='Password'/>
+             
+                <select className='select-user'>
+                <option value="grapefruit">Student</option>
+                <option value="lime">Administrator</option>
+                </select>
+
+                <input type="submit" value="Submit" />
+                </form>
+
+
+    </div>
+
+
         </div>
+
     );
 }
 
-export default logIn;
+export default LogIn;
